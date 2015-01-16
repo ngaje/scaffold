11a18e2576ddc2b785f818f779a95433ddd714b4
Russell Walker <info@netshine.co.uk>
2015-01-16 09:46:36 +0000

    ip address check

--- src/Request.php
+++ src/Request.php
@@ -106,6 +106,37 @@ class Request
         }
     }
 
+    public function getClientIpAddress()
+    {
+        $ip = "";
+        if (isset($_SERVER)) {
+            if (isset($_SERVER["HTTP_X_FORWARDED_FOR"])) {
+                $ip = $_SERVER["HTTP_X_FORWARDED_FOR"];
+                if (strpos($ip, ",") !== false) {
+                  $ip_array = explode(",", $ip);
+                  $ip = $ip_array[0];
+                }
+            } else if (isset($_SERVER["HTTP_CLIENT_IP"])) {
+                $ip = $_SERVER["HTTP_CLIENT_IP"];
+            } else {
+                $ip = $_SERVER["REMOTE_ADDR"];
+            }
+        } else {
+            if (getenv('HTTP_X_FORWARDED_FOR')) {
+                $ip = getenv('HTTP_X_FORWARDED_FOR');
+                if (strpos($ip, ",") !== false) {
+                  $ip_array = explode(",", $ip);
+                  $ip = $ip_array[0];
+                }
+            } else if (getenv('HTTP_CLIENT_IP')) {
+                $ip = getenv('HTTP_CLIENT_IP');
+            } else {
+                $ip = getenv( 'REMOTE_ADDR' );
+            }
+        }
+        return $ip;
+    }
+
     public function getHeader($param, $default_value = null)
     {
         $headers = array();
