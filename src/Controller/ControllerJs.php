5fafbd5d4b99a13387ed5d7a4429109935b8462a
Russell Walker <info@netshine.co.uk>
2015-09-01 12:38:01 +0100

    Replace tokens in file object instead of controller

--- src/Controller/ControllerJs.php
+++ src/Controller/ControllerJs.php
@@ -11,7 +11,7 @@ class ControllerJs extends ControllerBase
         $file = new File($full_file_name, 'text/javascript');
         if ($file->validateFileExtension(array('.js', '.javascript', '.css'), '.js') && $file->exists()) {
             if ($this->request->getRequestParam('tokenized')) {
-                $this->replaceTokens($file);
+                $file->replaceLanguageAndPHPTokens($this->request->language);
             }
             if (pathinfo($full_file_name, PATHINFO_EXTENSION) == 'css') {
                 $file->setContentType('text/css');
@@ -32,42 +32,5 @@ class ControllerJs extends ControllerBase
 
         $full_file_name = realpath(dirname(__FILE__) . '/..') . '/View/js/' . $js_file;
         return $full_file_name;
-    }
-
-    protected function replaceTokens(File $file)
-    {
-        $loop_breaker = 0;
-        $contents = $file->getContents();
-        $token_pos = strpos($contents, '[[');
-
-        while($token_pos !== false) {
-            $loop_breaker++;
-            if ($loop_breaker > 200) {
-                break;
-            }
-            $token_end = strpos($contents, ']]', $token_pos);
-            $language_resource = trim(substr($contents, $token_pos + 2, $token_end - ($token_pos + 2)));
-            list($resource, $entry) = explode(":", $language_resource);
-            $language_data = $this->request->language->{$resource}[$entry];
-            $contents = substr($contents, 0, $token_pos) . $language_data . substr($contents, $token_end + 2);
-            $token_pos = strpos($contents, '[[');
-        }
-
-        $token_pos = strpos($contents, '<' . '?php ');
-        while($token_pos !== false) {
-            $loop_breaker++;
-            if ($loop_breaker > 200) {
-                break;
-            }
-            $token_end = strpos($contents, '?' . '>', $token_pos + 1);
-            $php_code = trim(substr($contents, $token_pos + 6, $token_end - ($token_pos + 6)));
-            ob_start();
-            $result = eval($php_code);
-            $output = ob_get_clean();
-            $output = is_string($result) && strlen($result) > 0 && $output == '' ? $result : $output;
-            $contents = substr($contents, 0, $token_pos) . $output . substr($contents, $token_end + 2);
-            $token_pos = strpos($contents, '<' . '?php ');
-        }
-        $file->tempSetContents($contents);
     }
 }
--- src/File.php
+++ src/File.php
@@ -1,6 +1,8 @@
 <?php
 namespace Netshine\Scaffold;
 
+use Netshine\Scaffold\Language;
+
 class File
 {
     /** @var string **/
@@ -164,5 +166,56 @@ class File
     public function getName()
     {
         return $this->file_name;
+    }
+
+    public function replaceLanguageAndPHPTokens(Language $language)
+    {
+        $this->replaceLanguageTokens($language);
+        $this->replacePHPTokens();
+    }
+
+    public function replaceLanguageTokens(Language $language)
+    {
+        $loop_breaker = 0;
+        $contents = $this->getContents();
+        $token_pos = strpos($contents, '[[');
+
+        while($token_pos !== false) {
+            $loop_breaker++;
+            if ($loop_breaker > 200) {
+                break;
+            }
+            $token_end = strpos($contents, ']]', $token_pos);
+            $language_resource = trim(substr($contents, $token_pos + 2, $token_end - ($token_pos + 2)));
+            list($resource, $entry) = explode(":", $language_resource);
+            $language_data = $language->{$resource}[$entry];
+            $contents = substr($contents, 0, $token_pos) . $language_data . substr($contents, $token_end + 2);
+            $token_pos = strpos($contents, '[[');
+        }
+        $this->tempSetContents($contents);
+    }
+
+    public function replacePHPTokens()
+    {
+        $loop_breaker = 0;
+        $contents = $this->getContents();
+        $token_pos = strpos($contents, '[[');
+
+        $token_pos = strpos($contents, '<' . '?php ');
+        while($token_pos !== false) {
+            $loop_breaker++;
+            if ($loop_breaker > 200) {
+                break;
+            }
+            $token_end = strpos($contents, '?' . '>', $token_pos + 1);
+            $php_code = trim(substr($contents, $token_pos + 6, $token_end - ($token_pos + 6)));
+            ob_start();
+            $result = eval($php_code);
+            $output = ob_get_clean();
+            $output = is_string($result) && strlen($result) > 0 && $output == '' ? $result : $output;
+            $contents = substr($contents, 0, $token_pos) . $output . substr($contents, $token_end + 2);
+            $token_pos = strpos($contents, '<' . '?php ');
+        }
+        $this->tempSetContents($contents);
     }
 }
