d9a73e1f5cd7914e13de5abac2a028db0fb249ca
ngaje <2965497+ngaje@users.noreply.github.com>
2022-01-07 19:14:33 +0000

    Update FieldRendererSlideSwitch.php

--- src/View/Form/FieldTypes/FieldRendererSlideSwitch.php
+++ src/View/Form/FieldTypes/FieldRendererSlideSwitch.php
@@ -29,7 +29,8 @@ class FieldRendererSlideSwitch extends FieldRenderer
     protected function renderControl($type = null, $confirmation = false)
     {
         ?>
-	        <label class="field-control fld-slide-switch <?php echo $this->field->css_class; ?>">
+            <label class="slider-label"><?php echo $this->field->attributes['label']; ?></label>	        
+		<label class="field-control fld-slide-switch <?php echo $this->field->css_class; ?>">
 		        <input type="checkbox" name="<?php echo $this->field->name; ?>" <?php $this->outputId(null, '0'); ?> value="<?php echo $this->field->getValue($confirmation)  ;?>" <?php if ($this->field->value) {echo ' checked="checked"';} ?> <?php $this->outputAttributes($this->field->attributes); ?>>
 		        <span class="switch-slider" title="<?php echo $this->field->attributes['title']; ?>"></span>
 	        </label>
