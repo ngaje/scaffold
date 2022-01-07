5ae860740454669d4e6d9c86aa6f1e3c8cd7967b
ngaje <2965497+ngaje@users.noreply.github.com>
2022-01-07 10:12:30 +0000

    Add files via upload

---
+++ src/View/Form/FieldTypes/FieldRendererSlideSwitch.php
@@ -0,0 +1,38 @@
+<?php
+namespace Netshine\Scaffold\View\Form\FieldTypes;
+
+use Netshine\Scaffold\View\Form\FieldRenderer;
+
+class FieldRendererSlideSwitch extends FieldRenderer
+{
+    public function render()
+    {
+        if ($this->field->published) {
+            $this->loadJsFiles();
+            $this->preRender();
+            $this->renderErrorMessage();
+            $this->renderPreControl();
+            $this->renderControl();
+            $this->renderPostControl();
+            $this->renderCaption();
+            if ($this->field->request_confirmation) {
+                $this->renderPreControl(true);
+                $this->renderControl(null, true);
+                $this->renderPostControl(true);
+                $this->renderCaption(true);
+            }
+            $this->renderHelpText();
+            $this->postRender();
+        }
+    }
+
+    protected function renderControl($type = null, $confirmation = false)
+    {
+        ?>
+	        <label class="field-control fld-slide-switch <?php echo $this->field->css_class; ?>">
+		        <input type="checkbox" name="<?php echo $this->field->name; ?>" <?php $this->outputId(null, '0'); ?> value="<?php echo $this->field->getValue($confirmation)  ;?>" <?php if ($this->field->value) {echo ' checked="checked"';} ?> <?php $this->outputAttributes($this->field->attributes); ?>>
+		        <span class="switch-slider" title="<?php echo $this->field->attributes['title']; ?>"></span>
+	        </label>
+	    <?php
+    }
+}
