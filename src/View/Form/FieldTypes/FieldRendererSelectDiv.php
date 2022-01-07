a28a97287a4e27a1939639a2912597c4827205fa
ngaje <2965497+ngaje@users.noreply.github.com>
2022-01-07 11:47:52 +0000

    Update FieldRendererSelectDiv.php

--- src/View/Form/FieldTypes/FieldRendererSelectDiv.php
+++ src/View/Form/FieldTypes/FieldRendererSelectDiv.php
@@ -1,7 +1,7 @@
 <?php
-namespace Netshine\Scaffold\View\Form\FieldTypes;
+namespace Ngaje\Scaffold\View\Form\FieldTypes;
 
-use Netshine\Scaffold\View\Form\FieldRenderer;
+use Ngaje\Scaffold\View\Form\FieldRenderer;
 
 class FieldRendererSelectDiv extends FieldRenderer
 {
