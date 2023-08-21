b8c7acfe7be1cda68b381d3cd8db4c5843d3f758
ngaje <2965497+ngaje@users.noreply.github.com>
2023-08-21 15:59:27 +0100

    Update FieldRendererLinkButton.php to PHP8.1

--- src/View/Form/FieldTypes/FieldRendererLinkButton.php
+++ src/View/Form/FieldTypes/FieldRendererLinkButton.php
@@ -14,7 +14,7 @@ class FieldRendererLinkButton extends FieldRenderer
 
     public function renderControl($type = null, $confirmation = false)
     {
-        if (strpos($this->field->url, '[return]') !== false) {
+        if ($this->field->url && strpos($this->field->url, '[return]') !== false) {
             $this->field->url = str_replace('[return]', $this->field->parent_field_set->parent_form->return_url, $this->field->url);
         }
         if ($this->field->url) {
@@ -23,7 +23,7 @@ class FieldRendererLinkButton extends FieldRenderer
         }
         if ($this->field->image) {
             ?>
-            <a href="<?php echo $this->field->url; ?>"class="field-control fld-<?php echo $this->field->type; ?> title="<?php echo $this->field->value; ?>" <?php echo $this->field->css_class; ?>" <?php $this->outputId($confirmation ? 'confirm_' : ''); $this->outputAttributes($this->field->attributes); ?>>
+            <a href="<?php echo $this->field->url; ?>" class="field-control fld-<?php echo $this->field->type; ?> title="<?php echo $this->field->value; ?>" <?php echo $this->field->css_class; ?>" <?php $this->outputId($confirmation ? 'confirm_' : ''); $this->outputAttributes($this->field->attributes); ?>>
                 <img src="<?php echo $this->field->image; ?>" alt="<?php echo $this->field->value; ?>" border="0" />
                 <?php
                 if ($this->field->caption) {
